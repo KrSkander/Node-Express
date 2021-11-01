74e164a91696865e10a3b921878d38a24879f4e3
Kratou <kr.skander.k@gmail.com>
2021-11-01 12:11:10 +0100

    Handled get & post requests

---
+++ lab1/File.js
@@ -0,0 +1,50 @@
+
+var students = [ {
+    id : 1,
+    firstname : "someone",
+    lastname : "something",
+    email : "ye",
+    class : "1b1"
+},{
+    id : 2,
+    firstname : "someoneelse",
+    lastname : "somethingelse",
+    email : "yeye",
+    class : "1b2"
+}
+
+];
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/students', (req, res) => {
+  res.send(
+      students
+  )
+})
+
+app.post('/students',(req,res) => {
+    console.log(req.body);
+    let newStudent = req.body;
+    students.push(newStudent);
+    res.sendStatus(201);
+})
+
+// app.delete('/students',(req,res)=> {
+//     res.send(
+//         for( var i = 0; i < students.length; i++){ 
+    
+//             if ( students[i].id === 5) { 
+        
+//                 students.splice(i, 1); 
+//             }
+        
+//         }
+//     )
+// })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
